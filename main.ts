7f9e7b5f7d59be6cdb9b42e2f20edc6a1cf0a1a4
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-05-08 15:45:14 +0330

    feat: add basic logic

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+import { parseArgs } from '@std/cli';
+
+const DRIVER_CONFIG_PATH = '/sys/devices/platform/msi-ec/cooler_boost';
+
+const flags = parseArgs(Deno.args, {
+  string: ['set-state'],
+});
+
+function handleFatalError(error: unknown, msg: string) {
+  console.error(msg + '!', error);
+  Deno.exit(1);
+}
+
 if (import.meta.main) {
-    console.log('Hello World!');
+  try {
+    if (!Deno.args.length) console.log((await Deno.readTextFile(DRIVER_CONFIG_PATH)).trim());
+  } catch (error) {
+    handleFatalError(error, 'Failed to get cooler boost status');
+  }
+
+  const state = flags['set-state'];
+  if (state === 'on' || state === 'off') {
+    try {
+      await Deno.writeTextFile(DRIVER_CONFIG_PATH, state);
+    } catch (error) {
+      handleFatalError(error, 'Failed to change cooler boost status');
+    }
+  }
 }
